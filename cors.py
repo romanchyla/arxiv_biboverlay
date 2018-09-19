97027158ae62888c9e4d175cd986a46e1adcfa77
Roman Chyla <roman.chyla@gmail.com>
2018-09-19 17:39:22 -0400

    Updates

--- cors.py
+++ cors.py
@@ -2,7 +2,8 @@
 from flask_cors import CORS
 from abovl.app import create_app
 
+application = create_app()
+cors = CORS(application, allow_headers=('Content-Type', 'Accept', 'Credentials', 'Authorization', 'X-BB-Api-Client-Version'), supports_credentials=True, origins=['http://localhost:8000'])
+
 if __name__ == '__main__':
-    app = create_app()
-    cors = CORS(app, allow_headers=('Content-Type', 'Accept', 'Credentials', 'Authorization', 'X-BB-Api-Client-Version'), supports_credentials=True, origins=['http://localhost:8000'])
-    app.run('0.0.0.0', port=5000, debug=True)
+    application.run('0.0.0.0', port=5000, debug=True)
